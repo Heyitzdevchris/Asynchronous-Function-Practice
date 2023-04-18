8ad794c268b0eec50fe56870e2c5030caa24045d
Chris <chrisaaron184@gmail.com>
2023-04-18 16:09:22 -0700

    Update download function

--- download-and-process-video.js
+++ download-and-process-video.js
@@ -1,9 +1,10 @@
-function download(url) {
+function download(url, callback) {
   console.log(`Start downloading video from ${url} ...`);
 
   setTimeout(() => {
     const fileName = url.split("/").pop();
     console.log(`Video downloaded from ${url} to ${fileName}.`);
+    callback(fileName);
   }, 2000);
 }
 
@@ -17,6 +18,4 @@ function process(videoFile) {
 
 const url = "https://www.thinkful.com/sync-and-async.mov";
 
-const fileName = download(url);
-
-process(fileName);
+download(url, process);
